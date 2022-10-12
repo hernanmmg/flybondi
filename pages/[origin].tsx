293c1c97ab9066aa512a1edc000b602cf8fcb0d9
Hernan Medina <medinahm@globalhitss.com>
2022-10-11 23:44:55 -0500

    IntersectionObserver, sort y limit

--- pages/[origin].tsx
+++ pages/[origin].tsx
@@ -1,8 +1,7 @@
 import {ParsedUrlQuery} from "node:querystring";
 
 import {GetStaticPaths, GetStaticProps} from "next";
-import React from "react";
-import Head from "next/head";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 
 import api from "../api";
 
@@ -22,7 +21,7 @@ export const getStaticProps: GetStaticProps<Props, Params> = async ({params}) =>
 
   return {
     props: {
-      trips: trips.slice(0, 100),
+      trips: trips,
     },
   };
 };
@@ -35,27 +34,69 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 const OriginPage: React.FC<Props> = ({trips}) => {
+  const [sort, setSort] = useState<"days" | "price">("price");
+  const [limit, setLimit] = useState<number>(10);
+
+  const matches = useMemo(
+    () => [...trips].sort((a, b) => a[sort] - b[sort]).slice(0, limit),
+    [sort, trips, limit],
+  );
+
+  const checkpoint = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setLimit((limit) => limit + 10);
+      }
+    });
+
+    if (checkpoint.current) {
+      observer.observe(checkpoint.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>Destino</td>
-          <td>Días</td>
-          <td>Precio</td>
-        </tr>
-      </thead>
-      <tbody>
-        {trips.map((trip) => (
-          <tr key={trip.id}>
-            <td>{trip.origin.destination}</td>
-            <td>{trip.days}</td>
-            <td>
-              {Math.ceil(trip.price).toLocaleString("en-US", {currency: "USD", style: "currency"})}
+    <>
+      <table>
+        <thead>
+          <tr>
+            <td>Destino</td>
+            <td
+              style={{color: sort === "days" ? "yellow" : "inherit"}}
+              onClick={() => setSort("days")}
+            >
+              Días
+            </td>
+            <td
+              style={{color: sort === "price" ? "yellow" : "inherit"}}
+              onClick={() => setSort("price")}
+            >
+              Precio
             </td>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {matches.map((trip) => (
+            <tr key={trip.id}>
+              <td>{trip.origin.destination}</td>
+              <td>{trip.days}</td>
+              <td>
+                {Math.ceil(trip.price).toLocaleString("en-US", {
+                  currency: "USD",
+                  style: "currency",
+                })}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div ref={checkpoint} />
+    </>
   );
 };
 
